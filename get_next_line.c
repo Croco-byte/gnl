901a8de82680eaa3e8e706014abaa6baff9e3a8c
qroland <quentin.roland@sciencespo.fr>
2020-11-26 14:15:18 +0100

    Dynamically allocated buff to handle larger BUFFER_SIZES V1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 13:06:56 by user42            #+#    #+#             */
-/*   Updated: 2020/11/26 13:38:22 by user42           ###   ########.fr       */
+/*   Updated: 2020/11/26 14:12:00 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,12 +49,13 @@ int		malloc_fails(char **line, char *save, int *last_line)
 int		get_next_line(int fd, char **line)
 {
 	static char		*save = 0;
-	char			buff[BUFFER_SIZE + 1];
+	char			*buff;
 	int				reader;
 	int				last_line;
 
 	last_line = 0;
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+	if (fd < 0 || !line || BUFFER_SIZE <= 0 ||
+			!(buff = malloc(sizeof(char) * BUFFER_SIZE + 1)))
 		return (-1);
 	reader = 1;
 	while (has_return(save) != 1 && reader != 0)
@@ -64,6 +65,7 @@ int		get_next_line(int fd, char **line)
 		buff[reader] = '\0';
 		save = ft_strjoin(save, buff);
 	}
+	free (buff);
 	*line = line_from_save(save);
 	save = update_save(save, &last_line);
 	if (malloc_fails(line, save, &last_line))
